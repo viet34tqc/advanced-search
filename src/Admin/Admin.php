752f1523d7c4f5def95b55128b690f165725d99a
viet34tqc <viet34tqc@gmail.com>
2021-04-20 17:15:16 +0700

    Handle save setting

--- src/Admin/Admin.php
+++ src/Admin/Admin.php
@@ -25,19 +25,83 @@ class Admin {
 
 	public function register_setting() {
 		register_setting(
-			'nv-search-settings',
-			'nv-search-settings',
-			[
-				'sanitize_callback' => [ $this, 'validate_setting' ]
-			]
+			'nv_search_option_group',
+			'nv_search_settings',
 		);
 	}
 
 	public function register_fields() {
-		
+		add_settings_section(
+			'post_types',
+			'',
+			[ $this, 'render_section' ],
+			'nv_search_page'
+		);
+
+		add_settings_field(
+			'post_types',
+			'Post types',
+			[ $this, 'render_checkbox_fields' ],
+			'nv_search_page',
+			'post_types',
+		);
 	}
 
-	private function validate_setting( $input ) {
+	public function get_post_types() {
+		$unsupported = [
+			// WordPress built-in post types.
+			'customize_changeset',
+			'custom_css',
+			'nav_menu_item',
+			'oembed_cache',
+			'revision',
+			'user_request',
+			'wp_block',
+
+			// Meta Box post types.
+			'mb-post-type',
+			'mb-taxonomy',
+			'mb-relationship',
+			'mb-settings-page',
+			'mb-views',
+			'meta-box',
+		];
+		$post_types = get_post_types( [], 'objects' );
+		$post_types = array_diff_key( $post_types, array_flip( $unsupported ) );
+
+		return $post_types;
+	}
+
+	public function render_section() {
+		?>
+		<h4 class="nav-tab-wrapper"><?php echo esc_html( 'Select Post Types to Include in the Advanced Search', $this->plugin_text_domain ); ?></h4>
+		<?php
+	}
+
+	public function render_checkbox_fields() {
+		$post_types = $this->get_post_types();
+		$settings = get_option( 'nv_search_settings' );
+		foreach( $post_types as $post_type ) :
+			$name    = $post_type->name;
+			$label   = $post_type->label;
+			$checked = ! empty( $settings['post_type'] ) ? in_array( $name, $settings['post_type'] ) : false;
+		?>
+			<div>
+				<label for="<?php echo esc_attr( 'post_types' . '_' . $name ); ?>">
+					<input
+						type="checkbox"
+						id="<?= esc_attr( 'post_types' . '_' . $name ); ?>"
+						name="nv_search_settings[post_type][]"
+						value="<?php echo esc_attr( $name ); ?>"
+						<?php checked( $checked, 1, true ); ?>
+					/>
+					<?= esc_html( $label ); ?>
+				</label>
+			</div>
+		<?php endforeach;
+	}
+
+	public function validate_setting( $input ) {
 
 	}
 }
